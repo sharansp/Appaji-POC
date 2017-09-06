9039d9ca2cd902fad8b38dbf68293b70d0c75d55
sharan <sharangowda2425@gmail.com>
2017-09-06 18:06:33 +0530

    Abhishek tab

--- WebContent/Controller/Abhishekam.controller1.js
+++ WebContent/Controller/Abhishekam.controller1.js
@@ -1,36 +1,36 @@
-sap.ui.controller("com.test.Controller.Abhishekam", {
+sap.ui.controller("com.test.Controller.DivyaShakti.MurugharajAppaji", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf loginapp.App
+* @memberOf View.VideosHTML
 */
-	onInit: function() {
-		var page = this.oView.byId("idAbhishekamDetailView");
-		  if(sap.ui.Device.system.phone){
-				page.setShowNavButton(true);
-			}
-		/*var i=0,tile="";
-		tile = new sap.m.FormattedText ({
-			htmlText: "<h3 style=\"color:green; font-weight:600;\">Contacts</h3>" 
+	onInit: function(e) {
+		var that = this;
+		var oView = e.getSource().byId('idMurugharajAppajiView');
+		oView.setShowNavButton(true);
+		oView.attachNavButtonPress(function(navPress) {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(that);
+			//oRouter.navTo("Login");
+			
+			var oHistory = sap.ui.core.routing.History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
 
-		})
-		
-		var vbox = new sap.m.VBox();
-		vbox.addItem(tile);
-		if(page)
-		page.addContent(vbox);*/
-		//var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-		//oRouter.getRoute("StudentSessions").attachPatternMatched(this._onObjectMatched, this);
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				oRouter.navTo("DivyaShakti", true);
+			}
+		});
 	},
 	
-		
-	
-
+	navButtonPress: function() {
+		alert("?");
+	},
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf loginapp.App
+* @memberOf View.VideosHTML
 */
 //	onBeforeRendering: function() {
 //
@@ -39,7 +39,7 @@ sap.ui.controller("com.test.Controller.Abhishekam", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf loginapp.App
+* @memberOf View.VideosHTML
 */
 //	onAfterRendering: function() {
 //
@@ -47,7 +47,7 @@ sap.ui.controller("com.test.Controller.Abhishekam", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf loginapp.App
+* @memberOf View.VideosHTML
 */
 //	onExit: function() {
 //
